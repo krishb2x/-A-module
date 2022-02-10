f681adaa58369278ff740873868a7646efae825c
krishb2x <96863195+krishb2x@users.noreply.github.com>
2022-02-10 17:06:09 +0530

    Create  A_module.py

---
+++ A_module.py
@@ -0,0 +1,15 @@
+
+valid_sequence_dict = { "P1": "complete protein", \
+ "F1": "protein fragment", "DL": "linear DNA", "DC": "circular DNA", \
+ "RL": "linear RNA", "RC":"circular RNA", "N3": "transfer RNA", \
+ "N1": "other" }
+def find_valid_key(e):
+   for key,value in valid_sequence_dict.items():
+             if value == e:
+                 return key
+
+
+
+
+#you can use it by loading it:
+#import ValSeq
